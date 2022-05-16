5d880b2b1353b7c2f95defe9130179c51d1fc068
Manny <pineiroemmanuel@gmail.com>
2022-05-16 16:47:11 -0500

    added verbose error message

--- tests/test_kyu_six.py
+++ tests/test_kyu_six.py
@@ -7,41 +7,41 @@ class TestToCamelCase(TestCase):
     def test_to_camel_case_empty(self):
         result = to_camel_case("")
         expected = ""
-        assert result == expected
+        self.assertEqual(result, expected, f"expected: {expected}\nreceived: {result}")
 
     def test_to_camel_case_snake(self):
         result = to_camel_case("the_stealth_warrior")
         expected = "theStealthWarrior"
-        assert result == expected
+        self.assertEqual(result, expected, f"expected: {expected}\nreceived: {result}")
 
     def test_to_camel_case_dash(self):
         result = to_camel_case("The-Stealth-Warrior")
         expected = "TheStealthWarrior"
-        assert result == expected
+        self.assertEqual(result, expected, f"expected: {expected}\nreceived: {result}")
 
     def test_to_camel_case_screaming_dash(self):
         result = to_camel_case("A-B-C")
         expected = "ABC"
-        assert result == expected
+        self.assertEqual(result, expected, f"expected: {expected}\nreceived: {result}")
 
 
 class TestDuplicateEncode(TestCase):
     def test_duplicate_encode_no_dupes(self):
         result = duplicate_encode("din")
         expected = "((("
-        assert result == expected
+        self.assertEqual(result, expected, f"expected: {expected}\nreceived: {result}")
 
     def test_duplicate_encode_some_dupes(self):
         result = duplicate_encode("recode")
         expected = "()()()"
-        assert result == expected
+        self.assertEqual(result, expected, f"expected: {expected}\nreceived: {result}")
 
     def test_duplicate_encode_case_sensitive(self):
         result = duplicate_encode("Success")
         expected = ")())())"
-        assert result == expected
+        self.assertEqual(result, expected, f"expected: {expected}\nreceived: {result}")
 
     def test_duplicate_encode_special_chars(self):
         result = duplicate_encode("(( @")
         expected = "))(("
-        assert result == expected
+        self.assertEqual(result, expected, f"expected: {expected}\nreceived: {result}")
