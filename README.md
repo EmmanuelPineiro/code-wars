fe3770274eddbb96e1a416b2400c4740a2763871
Manny <pineiroemmanuel@gmail.com>
2024-03-05 16:52:03 -0800

    remove vital pieces of code

--- README.md
+++ README.md
@@ -8,14 +8,7 @@ examples
 1. once you've opened the project in pycharm, navigate to `file` > `settings` > `tools` > `python integrated tools`
 2. make sure your `default test runner` is set to `Unittests` or `Autodetect (Unittests)`
     1. If you have to change your test runner, you might get a message about your project. Just click the fix button  
-       <img alt="settings" src="/admin/image/settings_unit_tests.png"/>
-3. to create a new function, copy below and paste it to the bottom of `kyu_six.py` OR follow the same format to create a
-new kyu file. Below is the minimum you should have.
-   ```python
-   def function_name(arg): # pick a name that makes sense
-      """kata prompt"""
-      # your code here       
-      return False
+       <img alt="\
    ```
 4. once you've added your first function to your new file, move the cursor to the function name and hit `ctrl` + `shift` + `t`
 5. select `Create New Test...`, update the class name to be your function name in CamelCase, and click `ok` on the popup box  
--- src/kyu_six.py
+++ src/kyu_six.py
@@ -7,39 +7,11 @@ def to_camel_case(text):
 
     Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first
     word within the output should be capitalized only if the original word was capitalized (known as Upper Camel
-    Case, also often referred to as Pascal case). Examples
-
-    "the-stealth-warrior" gets converted to "theStealthWarrior"
-    "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
-
-
-    :param text: the text to convert to camel case
-    :return: the text as camel case
-    """
-    result = ''
-    if text:
+    Case, also ofasdfasdf
         words = re.split("[-_]", text)
 
         for word in words:
-            if result:
-                result += word[:1].upper() + word[1:]
-            else:
-                result += word
-
-    return result
-
-
-def duplicate_encode(word):
-    """
-    https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/python
-
-    The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if
-    that character appears only once in the original string, or ")" if that character appears more than once in the
-    original string. Ignore capitalization when determining if a character is a duplicate.
-
-    Examples
-
-    "din"      =>  "((("
+          asdf
     "recede"   =>  "()()()"
     "Success"  =>  ")())())"
     "(( @"     =>  "))(("
